5c18c4cb3f88c2ac4984001843c72b4f6039e75c
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-07-24 11:51:24 +0530

    ui

---
+++ 02-js/01-js-basics.js
@@ -0,0 +1,48 @@
+
+// JS Basics 
+
+// num1 = 10;
+// var num2 = 20;
+
+const num3 = 30;
+let num4 = 40;
+
+console.log(num3);
+console.log(num4);
+// num3 = 35;
+num4 = 45;
+console.log(num3);
+console.log(num4);
+
+
+// HTML5, CSS3, JS ES6 
+
+// ECMAScript - ES 6 
+// clss, object of a class, OOP, function declaration, async, await ... 
+
+// const employee = { eid: 101, firstName: 'Sonu' };
+
+// datatypes, declare variables, operators, syntax, function declaration, 
+
+// primitive types - number, string, boolean
+
+// JS object 
+const employee = {
+    eid: 101,
+    firstName: 'Sonu',
+    isMarried: false,
+    phones: [9876543210, 6789012345],
+    address: {
+        city: 'Pune',
+        pin: 400001
+    }
+};
+
+console.log(employee.address.city);
+
+
+
+
+
+
+
---
+++ 02-js/02-js-functions.js
@@ -0,0 +1,64 @@
+
+// Functions in JS 
+
+// function fun() {
+//     console.log('fun function');
+// }
+
+// fun();
+
+
+// const addNums = (i, j) => {
+//     console.log(i + j);
+// };
+
+// addNums(); // NaN 
+// addNums(10); // NaN
+// addNums(10, 20); // 30 
+// addNums(10, 20, 30); // 30 
+
+
+// console.log(11 + 12);
+// console.log("11" + 12);
+// console.log("11" + "12");
+// console.log(11 + "12");
+
+// dynamically typed language 
+// strongly typed language 
+
+// strongly typed language 
+// // java 
+// int num = 10;
+// num = 20;
+// num = "abc"; // error 
+
+// dynamically typed language 
+// JS 
+// let someValue;
+// console.log(someValue);
+// console.log(typeof (someValue));
+// someValue = 10;
+// console.log(someValue);
+// console.log(typeof (someValue));
+// someValue = "abc";
+// console.log(someValue);
+// console.log(typeof (someValue));
+
+// let num1 = 10;
+// let num2;
+
+// console.log(num1 + num2);
+
+// console.log("10" + 5);
+// console.log("10" + 5 - 2);
+// console.log("abc" - 5);
+// console.log("abc" - 5);
+
+const addNums = (i, j) => {
+    // return i + j;
+    console.log(i + j); 
+};
+
+let output = addNums(10, 20);
+console.log(output); // undefined 
+
