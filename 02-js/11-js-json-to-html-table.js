16d57c9b700ea9826830bfb06d0afc48ecdecb63
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-07-26 14:51:12 +0530

    ui-3

--- 02-js/11-js-json-to-html-table.js
+++ 02-js/11-js-json-to-html-table.js
@@ -1,13 +1,50 @@
 
+// const url = 'https://jsonplaceholder.typicode.com/users';
+// const userData = document.getElementById('user-data');
+// const tableData = document.getElementById('table-data');
+// const tableHeaders = document.getElementById('table-headers');
+
+// const getUserData = () => {
+//     let temp = '';
+//     let temp2 = '';
+//     let temp3 = '';
+
+//     fetch(url)
+//         .then((resp) => {
+//             return resp.json();
+//         })
+//         .then((res) => {
+//             res.forEach((user) => {
+//                 temp2 = user;
+//                 console.log(user);
+//                 temp += `<tr> 
+//                 <td>${user.id} </td>
+//                 <td>${user.name} </td>
+//                 <td>${user.username} </td>
+//                 <td>${user.email} </td>
+//                 <td>${user.address.city} </td>
+//                 <td>${user.phone} </td>
+//                 <td>${user.website} </td>
+//                 <td>${user.company.name} </td>
+//                 </tr>`;
+//             });
+
+//             Object.keys(temp2).forEach(elem => temp3 += `<th>${elem}</th>`);
+//             tableHeaders.innerHTML = temp3;
+//             tableData.innerHTML = temp;
+//         })
+//         .catch(err => console.log(err));
+// };
+
+// getUserData();
+
+
 const url = 'https://jsonplaceholder.typicode.com/users';
 const userData = document.getElementById('user-data');
 const tableData = document.getElementById('table-data');
-const tableHeaders = document.getElementById('table-headers');
 
 const getUserData = () => {
     let temp = '';
-    let temp2 = '';
-    let temp3 = '';
 
     fetch(url)
         .then((resp) => {
@@ -15,7 +52,6 @@ const getUserData = () => {
         })
         .then((res) => {
             res.forEach((user) => {
-                temp2 = user;
                 console.log(user);
                 temp += `<tr> 
                 <td>${user.id} </td>
@@ -28,9 +64,6 @@ const getUserData = () => {
                 <td>${user.company.name} </td>
                 </tr>`;
             });
-
-            Object.keys(temp2).forEach(elem => temp3 += `<th>${elem}</th>`);
-            tableHeaders.innerHTML = temp3;
             tableData.innerHTML = temp;
         })
         .catch(err => console.log(err));
