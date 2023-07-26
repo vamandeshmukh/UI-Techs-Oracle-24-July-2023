f608d52329907550e699ded5e4c8bedd1cdfe51f
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-07-26 14:28:18 +0530

    ui-3

--- 02-js/11-js-json-to-html-table.js
+++ 02-js/11-js-json-to-html-table.js
@@ -1,14 +1,23 @@
 
 const url = 'https://jsonplaceholder.typicode.com/users';
+const userData = document.getElementById('user-data');
 
-fetch(url)
-    .then((resp) => {
-        return resp.json();
-    })
-    .then((res) => {
-        res.forEach((user) => {
-            console.log(user);
-        });
-    })
-    .catch(err => console.log(err));
+const getUserData = () => {
+    let temp = '';
+
+    fetch(url)
+        .then((resp) => {
+            return resp.json();
+        })
+        .then((res) => {
+            res.forEach((user) => {
+                console.log(user);
+                temp += `<p>${user.username}</p>`;
+            });
+            userData.innerHTML = temp;
+        })
+        .catch(err => console.log(err));
+};
+
+getUserData();
 
