614edeb3f7ce5227f8da3664e25a6434172e40c3
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-07-26 14:46:15 +0530

    ui-3

--- 02-js/11-js-json-to-html-table.js
+++ 02-js/11-js-json-to-html-table.js
@@ -2,9 +2,12 @@
 const url = 'https://jsonplaceholder.typicode.com/users';
 const userData = document.getElementById('user-data');
 const tableData = document.getElementById('table-data');
+const tableHeaders = document.getElementById('table-headers');
 
 const getUserData = () => {
     let temp = '';
+    let temp2 = '';
+    let temp3 = '';
 
     fetch(url)
         .then((resp) => {
@@ -12,11 +15,22 @@ const getUserData = () => {
         })
         .then((res) => {
             res.forEach((user) => {
+                temp2 = user;
                 console.log(user);
-                temp += `<tr> <td>${user.name} </td>
+                temp += `<tr> 
+                <td>${user.id} </td>
+                <td>${user.name} </td>
                 <td>${user.username} </td>
-                <td>${user.email} </td></tr>`;
+                <td>${user.email} </td>
+                <td>${user.address.city} </td>
+                <td>${user.phone} </td>
+                <td>${user.website} </td>
+                <td>${user.cpmpany} </td>
+                </tr>`;
             });
+
+            Object.keys(temp2).forEach(elem => temp3 += `<th>${elem}</th>`);
+            tableHeaders.innerHTML = temp3;
             tableData.innerHTML = temp;
         })
         .catch(err => console.log(err));
