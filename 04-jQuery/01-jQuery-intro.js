d8a234a0e5142ce96ef40503b907f8141d5c81f7
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-07-27 10:31:55 +0530

    ui-4

--- 04-jQuery/01-jQuery-intro.js
+++ 04-jQuery/01-jQuery-intro.js
@@ -1,14 +1,14 @@
 
-// without jQuery 
+// // without jQuery 
 
-document.addEventListener('DOMContentLoaded', () => {
-    let button = document.getElementById('jq-demo');
-    let paragraph = document.getElementById('jq-para');
-    button.addEventListener('click', () => {
-        paragraph.textContent = 'Text changed without jQuery';
+// document.addEventListener('DOMContentLoaded', () => {
+//     let button = document.getElementById('jq-demo');
+//     let paragraph = document.getElementById('jq-para');
+//     button.addEventListener('click', () => {
+//         paragraph.textContent = 'Text changed without jQuery';
 
-    })
-});
+//     })
+// });
 
 // with jQuery 
 
@@ -16,12 +16,25 @@ $(document).ready(
     () => {
         $('#jq-demo').click(() => {
             $('#jq-para').text('Text changed with jQuery');
+            // $('#jq-para').hide();
         });
     }
 );
 
+// syntax
 // $(document).ready(
 //     () => {
-
+//     $('#element-id').eventName(() => {
+//         action to perform on the event
+//     });
 //     }
 // );
+
+
+
+
+
+
+
+
+
